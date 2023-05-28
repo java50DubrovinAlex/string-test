5a6ed8c9d567e3d43a1f1f9bb42f0776b16821d0
Alexander <caxagsh@gmail.com>
2023-05-28 18:07:11 +0300

    hw_01 last vre

--- src/StringTests.java
+++ src/StringTests.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class StringTests {
+	String str0 = "";
 	String str1 = "qwerty";
 	String str2 = "qwerty";
 	String str3 = "qwertz";
@@ -12,46 +13,47 @@ class StringTests {
 	@Test
 	void testCharAT() {
 		assertEquals('q', str1.charAt(0));
-		assertTrue(str1.charAt(1) == 'w' );
-		assertFalse(str1.charAt(5) != 'y');
+		assertEquals('r', str1.charAt(3));
+		assertEquals('y', str1.charAt(5));
 		
 	}
 	@Test
 	void testCompareTo() {
 		assertEquals(0, str1.compareTo(str2));
-		assertTrue(str1.compareTo(str3) < 0);
-		assertTrue(str1.compareTo(str4) > 0);
+		assertEquals(-25, str4.compareTo(str3));
+		assertEquals(25, str3.compareTo(str4));
 		
 	}
 	@Test
 	void testCompareToIgnoreCase() {
 		assertEquals(0, str1.compareToIgnoreCase(str5));
-		assertTrue(str1.compareToIgnoreCase(str3) < 0);
-		assertTrue(str1.compareToIgnoreCase(str4) > 0);
+		assertEquals(-25, str4.compareToIgnoreCase(str3));
+		assertEquals(25, str3.compareToIgnoreCase(str4));
 	}
 	@Test
 	void testConcat() {
 		assertEquals("qwertyQWERTY", str1.concat(str5));
+		assertEquals(str6, str0.concat(str6));
 	}
 	@Test
 	void testStarstWith() {
-		assertTrue(str1.startsWith("qwer"));
-		assertFalse(str1.startsWith("QWE"));
+		assertTrue(str6.startsWith("Hell"));
+		assertFalse(str6.startsWith("HELL"));
 	}
 	@Test
 	void testEndstWith() {
-		assertTrue(str1.endsWith("rty"));
-		assertFalse(str1.startsWith("RTY"));
+		assertTrue(str6.endsWith("planet."));
+		assertFalse(str6.startsWith("planet"));
 	}
 	@Test
 	void testContains() {
-		assertTrue(str1.contains("qwer"));
-		assertFalse(str1.contains("abc"));
+		assertTrue(str6.contains("you"));
+		assertFalse(str6.contains("not"));
 	}
 	@Test
 	void testIndexOf() {
 		assertEquals(6, str6.indexOf("planet"));
-		assertEquals(-1, str6.indexOf(str1));
+		assertEquals(-1, str6.indexOf("moon"));
 	}
 	@Test
 	void testLastIndexOf() {
